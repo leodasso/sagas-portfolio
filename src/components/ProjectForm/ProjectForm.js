209c631ac477d46d95c9446b009f63fd6ab1ddeb
Leo Dasso <leo.dasso@gmail.com>
2019-03-10 15:12:56 -0500

    in progress form submission functionality

--- src/components/ProjectForm/ProjectForm.js
+++ src/components/ProjectForm/ProjectForm.js
@@ -16,16 +16,60 @@ const styles = theme => ({
     }
 });
 
+const defaultState = {
+    name: '',
+    description: '',
+    date: '',
+    github: '',
+    website: '',
+    tags: '',
+}
+
 class ProjectForm extends Component {
 
-    createInput = (placeholder, inputType) => {
+    state = {
+        ...defaultState,
+    }
+
+    updateState = propName => event => {
+        this.setState({[propName]: event.target.value});
+    }
+
+    // Returns JSX for a styled Material UI input field.
+    createInput = (placeholder, inputType, stateName) => {
         return (
             <TextField 
                 variant="filled" type={inputType}
                 className={this.props.classes.textField} 
                 placeholder={placeholder} 
+                onChange={this.updateState(stateName)}
+                value={this.state[stateName]}
             />
         )
+    }
+
+    onSubmit = event => {
+        event.preventDefault();
+
+        console.log('submitted');
+        let required = [];
+        if (this.state.name === '' ) {
+            required.push('Name');
+        }
+        if (this.state.github === '') {
+            required.push('GitHub');
+        }
+        if (required.length > 0) {
+            alert('You need to fill in ' + required.join(', ') + ' to continue!');
+            return;
+        }
+
+        // submit the project here
+
+        // clear out the input fields
+        this.setState({
+            ...defaultState,
+        });
     }
 
     render() {
@@ -36,15 +80,19 @@ class ProjectForm extends Component {
             <div className="project-form">
                 <Typography variant="h4">Add New Project</Typography>
                 <Divider/>
+
+                {/* Not using the form 'onSubmit' here because it doesn't intercept MaterialUI Button clicks */}
                 <form className="input-container">
-                        {this.createInput('Name', 'text')}
-                        {this.createInput('10/31/2015', 'date')}
-                        {this.createInput('jQuery', 'text')}
-                        {this.createInput('GitHub URL', 'url')}
-                        {this.createInput('Website URL', 'url')}
+                        {this.createInput('Name', 'text', 'name')}
+                        {this.createInput('10/31/2015', 'date', 'date')}
+                        {this.createInput('jQuery', 'text', 'tags')}
+                        {this.createInput('GitHub URL', 'url', 'github')}
+                        {this.createInput('Website URL (optional)', 'url', 'website')}
                         <TextField variant="filled" className={classes.textField} multiline rows="4"
-                            fullWidth placeholder="description (optional)" />
-                    <Button className={classes.submitButton}>Submit</Button>
+                            fullWidth placeholder="description (optional)" value={this.state.descriptionsd}
+                            onChange={this.updateState('description')}
+                            />
+                    <Button onClick={this.onSubmit} className={classes.submitButton}>Submit</Button>
                 </form>
             </div>
         );
