bb9f118699b5466900ceb4c00669a7b9ea6f26bb
Leo Dasso <leo.dasso@gmail.com>
2019-03-10 12:50:14 -0500

    implement routes for main and admin pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Profile from '../Profile/Profile';
-import ProjectList from '../ProjectList/ProjectList';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Main from '../Main/Main';
+import Admin from '../Admin/Admin';
+ 
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
+      <Router>
       <div className="App">
-        <Profile />
-        <ProjectList />
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/admin" component={Admin}/>
       </div>
+      </Router>
     );
   }
 }
