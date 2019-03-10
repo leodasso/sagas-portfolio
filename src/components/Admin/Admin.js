76b41a7a67d4a8d6d57196d7776a5e07c8f11000
Leo Dasso <leo.dasso@gmail.com>
2019-03-10 18:20:18 -0500

    styling tweaks

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -9,45 +9,58 @@ import ProjectTable from '../ProjectTable/ProjectTable';
 // style for the material UI stuff.
 const styles = {
 
-    mainPaper: {
-        backgroundColor: '#edde5a',
-        margin: '44px',
-        minHeight: '300px',
-        padding: '24px',
-        paddingBottom: '34px',
-    }
+	pageContainer: {
+		margin: 34,
+	},
+
+	adminContainer: {
+		margin: 'auto',
+		maxWidth: 850,
+	},
+
+	mainPaper: {
+		backgroundColor: '#edde5a',
+		minHeight: '300px',
+		padding: '24px',
+		paddingBottom: '34px',
+	}
 
 }
 
 class Admin extends Component {
 
-    onClickBack = () => {
-        this.props.history.push('/');
-    }
+	onClickBack = () => {
+		this.props.history.push('/');
+	}
 
-    render() {
-        return (
-            <>
-            <div className="admin-title">
-                <h1 className="headline">Admin</h1>
-                <br/>
-                <Button 
-                    onClick={this.onClickBack}
-                    color="inherit"
-                    variant="outlined">
-                    Back to Projects
-                </Button>
-            </div>
-            <Paper className={this.props.classes.mainPaper}>
+	render() {
 
-                <ProjectForm />
-                <br/>
-                <ProjectTable />
-                <br/>
-            </Paper>
-            </>
-        );
-    }
+		const classes = this.props.classes;
+
+		return (
+			<div className={classes.pageContainer}>
+				<div className={classes.adminContainer}>
+					<div className="admin-title">
+						<h1 className="headline">Admin</h1>
+						<br/>
+						<Button 
+							onClick={this.onClickBack}
+							color="inherit"
+							variant="outlined">
+							Back to Projects
+						</Button>
+					</div>
+					<Paper className={classes.mainPaper}>
+
+						<ProjectForm />
+						<br/>
+						<ProjectTable />
+						<br/>
+					</Paper>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withStyles(styles)(Admin);
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -12,31 +12,40 @@ import CardContent from '@material-ui/core/CardContent';
 import {  Typography, Divider} from '@material-ui/core';
 
 const styles = {
-    projectCard: {
-        backgroundColor: '#edde5a',
-        display: 'flex',
-        minHeight: 200
-    },
-    info: {
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'left',
-        flexGrow: 1.5,
-    },
-    thumbnail: {
-        width: 150,
-        minWidth: 150,
-    },
-    links: {
-        marginTop: 2,
-        justifyContent: 'space-evenly',
-    },
-    buttonLink: {
-        flexGrow: 2,
-    },
-    button: {
-        width: '100%',
-    },
+	projectCard: {
+		backgroundColor: '#edde5a',
+		display: 'flex',
+		minHeight: 200
+	},
+	info: {
+		display: 'flex',
+		flexDirection: 'column',
+		textAlign: 'left',
+		flexGrow: 1.5,
+		margin: 0,
+	},
+	infoBody: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		flexDirection: 'column',
+		height: '100%',
+		margin: 0,
+	},
+	thumbnail: {
+		width: 150,
+		minWidth: 150,
+	},
+	links: {
+		marginTop: 2,
+		justifyContent: 'space-evenly',
+
+	},
+	buttonLink: {
+		flexGrow: 2,
+	},
+	button: {
+		width: '100%',
+	},
 }
 
 
@@ -44,67 +53,66 @@ const styles = {
 class Project extends Component {
 
 
-    componentWillMount() {
-        this.props.dispatch({type:'FETCH_TAGS'});
-    }
+	componentWillMount() {
+		this.props.dispatch({type:'FETCH_TAGS'});
+	}
 
-    /** Display a button. Checks if the link is empty or null, and if it is,
-     * will not display anything. If there is a link to display, it will use
-     * the display name as the 'visible to the user' text
-     */
-    conditionalRenderButton(displayName, link) {
-        if (link === '' || link === null ) {
-            return '';
-        }
+	/** Display a button. Checks if the link is empty or null, and if it is,
+	 * will not display anything. If there is a link to display, it will use
+	 * the display name as the 'visible to the user' text
+	 */
+	conditionalRenderButton(displayName, link) {
+		if (link === '' || link === null ) {
+			return '';
+		}
 
-        return <ProjectButton name={displayName} link={link} />;
-    }
+		return <ProjectButton name={displayName} link={link} />;
+	}
 
-    renderTag = () => {
-        const project = this.props.projectData;
-        const tag = this.props.tags[project.tag_id];
-        if (tag === null || tag === undefined) {
-            return '';
-        }
-        return <div className="tag">{tag.name}</div>;
-    }
+	renderTag = () => {
+		const project = this.props.projectData;
+		const tag = this.props.tags[project.tag_id];
+		if (tag === null || tag === undefined) {
+			return '';
+		}
+		return <div className="tag">{tag.name}</div>;
+	}
 
-    render() {
+	render() {
 
-        const classes = this.props.classes;
-        const project = this.props.projectData;
+		const classes = this.props.classes;
+		const project = this.props.projectData;
 
-        return (
-            <Grid item sm={12} md={6}>
-                <Card className={classes.projectCard}>
-                    <CardMedia
-                        className={classes.thumbnail}
-                        image="images/default_thumbnail.png"
-                        title="my happy project"
-                    />
-                    <CardContent className={classes.info}>
-                        <div className="project-header">
-                            <Typography variant="h5">{project.name}</Typography>
-                            {this.renderTag()}
-                        </div>
-                        <Divider />
-
-                        <Grid container spacing={8} className={classes.links}>
-
-                            {this.conditionalRenderButton('GitHub', project.github)}
-                            {this.conditionalRenderButton('Website', project.website)}
-                        </Grid>
-
-                        <p>{project.description}</p>
-                    </CardContent>
-                </Card>
-            </Grid>
-        );
-    }
+		return (
+			<Grid item sm={12} md={6}>
+				<Card className={classes.projectCard}>
+					<CardMedia
+						className={classes.thumbnail}
+						image="images/default_thumbnail.png"
+						title="my happy project"
+					/>
+					<CardContent className={classes.info}>
+						<div className="project-header">
+							<div className="title">{project.name}</div>
+							{this.renderTag()}
+						</div>
+						<Divider />
+						<div className={classes.infoBody}>
+							<p>{project.description}</p>
+							<Grid container spacing={8} className={classes.links}>
+								{this.conditionalRenderButton('GitHub', project.github)}
+								{this.conditionalRenderButton('Website', project.website)}
+							</Grid>
+						</div>
+					</CardContent>
+				</Card>
+			</Grid>
+		);
+	}
 }
 
 const reduxMap = reduxState => reduxState;
 
 export default  withStyles(styles)(
-                connect(reduxMap)(
-                    Project));
+				connect(reduxMap)(
+					Project));
