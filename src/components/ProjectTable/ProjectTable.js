6f903c5ee834aefa62aaa300943138f7dd71a5d6
Leo Dasso <leo.dasso@gmail.com>
2019-03-10 17:49:10 -0500

    add delete route

--- src/components/ProjectTable/ProjectTable.js
+++ src/components/ProjectTable/ProjectTable.js
@@ -3,62 +3,80 @@ import { connect } from 'react-redux';
 
 // material UI
 import {
-    Table, TableBody, TableRow, TableCell,
-    Button, TableHead, withStyles
+	Table, TableBody, TableRow, TableCell,
+	Button, TableHead, withStyles
 } from '@material-ui/core';
 
 // Custom style for the Material Table
 const CustomTableCell = withStyles(theme => ({
-    head: {
-        backgroundColor: '#d6c95c',
-        borderColor: 'rgba(0, 0, 0, .3)',
-    },
-    body: {
-        borderColor: 'rgba(0, 0, 0, .3)',
-    },
+	head: {
+		backgroundColor: '#d6c95c',
+		borderColor: 'rgba(0, 0, 0, .3)',
+	},
+	body: {
+		borderColor: 'rgba(0, 0, 0, .3)',
+	},
 }))(TableCell);
+
+
 
 class ProjectTable extends Component {
 
-    componentDidMount() {
-        // fetch the projects data when the component mounts
-        this.props.dispatch({ type: 'FETCH_PROJECTS' });
-    }
+	componentDidMount() {
+		// fetch the projects data when the component mounts
+		this.props.dispatch({ type: 'FETCH_PROJECTS' });
+	}
 
-    render() {
+	// Deletes the project with the given id. Dispatches
+	// an action to redux, where the saga DELETE_PROJECT takes care of the rest.
+	deleteProject = projectId => event => {
 
-        const projects = this.props.projects;
+		if (window.confirm('Are you sure you want to delete this project?')){
+			this.props.dispatch({
+				type:"DELETE_PROJECT",
+				payload: projectId,
+			});
+		}
+	}
 
-        return (
-            <div>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <CustomTableCell>Name</CustomTableCell>
-                            <CustomTableCell>Description</CustomTableCell>
-                            <CustomTableCell>Delete</CustomTableCell>
-                        </TableRow>
+	render() {
 
-                    </TableHead>
-                    <TableBody>
-                        {
-                            projects.map((project, index) => (
-                                <TableRow key={index}>
-                                    <CustomTableCell>{project.name}</CustomTableCell>
-                                    <CustomTableCell>{project.description}</CustomTableCell>
-                                    <CustomTableCell><Button>X</Button></CustomTableCell>
-                                </TableRow>
-                            ))
-                        }
-                    </TableBody>
-                </Table>
-            </div>
-        );
-    }
+		const projects = this.props.projects;
+
+		return (
+			<div>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<CustomTableCell>Name</CustomTableCell>
+							<CustomTableCell>Description</CustomTableCell>
+							<CustomTableCell>Delete</CustomTableCell>
+						</TableRow>
+
+					</TableHead>
+					<TableBody>
+						{
+							projects.map((project) => (
+								<TableRow key={project.id}>
+									<CustomTableCell>{project.name}</CustomTableCell>
+									<CustomTableCell>{project.description}</CustomTableCell>
+									<CustomTableCell>
+										<Button
+											onClick={this.deleteProject(project.id)}>
+											X</Button>
+									</CustomTableCell>
+								</TableRow>
+							))
+						}
+					</TableBody>
+				</Table>
+			</div>
+		);
+	}
 }
 
 const reduxMapper = reduxState => reduxState;
 
 export default connect(reduxMapper)(
-    withStyles()(
-        ProjectTable));
+	withStyles()(
+		ProjectTable));
--- src/index.js
+++ src/index.js
@@ -18,22 +18,47 @@ import axios from 'axios';
 // Saga for fetching project data from the server & updating the store
 function* fetchProjects() {
 
-    const response = yield axios.get('/portfolio');
-    yield dispatch({type:'SET_PROJECTS', payload: response.data});
+	try {
+		const response = yield axios.get('/portfolio');
+		yield dispatch({type:'SET_PROJECTS', payload: response.data});
+	}
+	catch (error) {
+		console.log('error fetching projects', error);
+	}
 }
 
+// Saga which fetches the list of tags (html, css, etc) from the server,
+// and then updates the store with them.
 function* fetchTags() {
-    const response = yield axios.get('/tags');
-    yield dispatch({type:'SET_TAGS', payload: response.data});
+	try{
+		const response = yield axios.get('/tags');
+		yield dispatch({type:'SET_TAGS', payload: response.data});
+	}
+	catch (error) {
+		console.log('error fetching tags', error);
+	}
 }
+
+function* deleteProject(action) {
+
+	try{
+		yield axios.delete('/portfolio/' + action.payload);
+		yield dispatch({type: 'FETCH_PROJECTS'});
+	}
+	catch (error) {
+		console.log('error deleting project with id ' + action.payload, error);
+	}
+}
+
 
 // Create the rootSaga generator function
 function* rootSaga() {
 
-    yield takeEvery('FETCH_PROJECTS', fetchProjects);
-    // every single project instance will call this action,
-    // so we only want to take the latest to fetch tags.
-    yield takeLatest('FETCH_TAGS', fetchTags);
+	yield takeEvery('FETCH_PROJECTS', fetchProjects);
+	// every single project instance will call this action,
+	// so we only want to take the latest to fetch tags.
+	yield takeLatest('FETCH_TAGS', fetchTags);
+	yield takeEvery('DELETE_PROJECT', deleteProject);
 }
 
 // Create sagaMiddleware
@@ -41,37 +66,37 @@ const sagaMiddleware = createSagaMiddleware();
 
 // Used to store projects returned from the server
 const projects = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_PROJECTS':
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case 'SET_PROJECTS':
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 // Used to store the project tags (e.g. 'React', 'jQuery', 'Angular', 'Node.js')
 const tags = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_TAGS':
-            return action.payload;
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case 'SET_TAGS':
+			return action.payload;
+		default:
+			return state;
+	}
 }
 
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        projects,
-        tags,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+	combineReducers({
+		projects,
+		tags,
+	}),
+	// Add sagaMiddleware to our store
+	applyMiddleware(sagaMiddleware, logger),
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
-    document.getElementById('root'));
+	document.getElementById('root'));
 registerServiceWorker();
